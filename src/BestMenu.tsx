afc5e10852caa013a12c6ce9b12f7785ba2d6759
Park Chan <qkrcks8028@gmail.com>
2023-08-04 19:14:06 +0900

    0804 수정

--- src/BestMenu.tsx
+++ src/BestMenu.tsx
@@ -5,7 +5,6 @@ interface OwnProps extends Omit<Menu, 'price'> {
     showBestMenuName(name:string):string
 }
 
-// 함수까지 받아올수있음 
 const BestMenu:React.FC<OwnProps> = ({name, showBestMenuName}) => {
   return (
     <div>{name}</div>
