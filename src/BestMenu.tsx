d7b6937d7a506b1049ca8a9f7ec15fa45fd01d30
Park Chan <qkrcks8028@gmail.com>
2023-08-08 10:26:59 +0900

    0808

--- src/BestMenu.tsx
+++ src/BestMenu.tsx
@@ -4,8 +4,6 @@ import { Menu } from './model/restaurant'
 interface OwnProps extends Omit<Menu, 'price'> {
     showBestMenuName(name:string):string
 }
-
-// 리액트 타입스크립트 변환입니다
 const BestMenu:React.FC<OwnProps> = ({name, showBestMenuName}) => {
   return (
     <div>{name}</div>
